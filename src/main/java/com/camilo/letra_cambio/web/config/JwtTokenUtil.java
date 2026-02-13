6e3d219b54e8229f49b9688880291d68d11c8a80
Camilo Soto V <olimpusmac@gmail.com>
2026-02-12 23:06:41 -0500

    bug: seed

--- src/main/java/com/camilo/letra_cambio/web/config/JwtTokenUtil.java
+++ src/main/java/com/camilo/letra_cambio/web/config/JwtTokenUtil.java
@@ -4,15 +4,18 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenUtil {
-    private static final String SEED = "Colombia1.";
+    @Value("${jwt.secret-key}")
+    private String secretKey;
 
     public String extractSubject(String token) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(SEED);
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
             JWTVerifier verifier = JWT.require(algorithm).build();
             String decodedToken = verifier.verify(token).getToken();
             return JWT.decode(decodedToken).getSubject();
